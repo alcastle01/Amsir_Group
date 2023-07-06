f9a582396f47408f719f22f4c7d1c824bb2f3b4a
Milton Sanchez <snchzmltn@hotmail.com>
2023-07-05 23:16:11 -0500

    continuew to increment styles expanded new sections of the page, with Dividers and a Grid in the end the idea is to continue with styles and incrementing content, adding img resources and proper links to partners

--- amsir-page/src/pages/WelcomePage/WelcomePage.tsx
+++ amsir-page/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,5 +1,7 @@
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
 
 function WelcomePage() {
 	return (
@@ -9,34 +11,100 @@ function WelcomePage() {
 			<br />
 			<div>
 		  		{/*What is amsir? Join us now!*/}
-				<Typography variant='h1'>
+				<Typography variant='h2'>
 					What is AMSIR?
 				</Typography>
 				<Typography sx={{
-					'padding-left': '10%',
-					'padding-right': '10%'
+					'margin-left': '10%',
+					'margin-right': '10%',
+					'padding-left': '1.5em',
+					'padding-right': '1.5em',
+					'border': '5px solid #A084DC',
+					'border-radius': '10%',
+					':hover': {
+						'background-color': '#fce6f9'
+					}
 				}} variant='body1' align='justify'>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus sed purus sed suscipit. Suspendisse sit amet lacinia risus, non ullamcorper justo. Proin ut lacus erat. Nam porta mattis auctor. Mauris et elit urna. Integer rhoncus metus ac congue suscipit. Cras ornare urna a sodales hendrerit. Ut commodo tellus metus, ultrices efficitur nunc elementum a. Nulla eros enim, elementum nec massa et, porta dignissim dolor. Praesent finibus nisi vitae tortor vestibulum aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi ut odio sit amet metus sollicitudin condimentum non non magna. Pellentesque ut lacus dolor. Vivamus fermentum, urna vitae tempor pharetra, lectus magna ultricies arcu, ac aliquet erat purus eu mi.
 				</Typography>
 				<br/>
-				<Typography variant='button' color='primary'>
-					<Button variant='outlined'>
+				<Button sx={{
+					'background-color': '#A084DC',
+					'margin': '0.5em',
+					':hover': {
+      					'background-color': '#645CBB',
+      					'color': 'white',
+    				},
+				}} variant='contained' size='large' >
 						Join us now!!
-					</Button>
-				</Typography>
+				</Button>
+				<br/>
 			</div>
+			<Divider orientation='horizontal' variant='middle' />
 			<div>
 		 		{/*Main courses*/}
-				<Typography variant='h1'>
+				<Typography variant='h2'>
 					Main courses
 				</Typography>
+				<ul>
+					<li>
+						<p>Curso 1</p>
+					</li>
+					<li>
+						<p>Curso 2</p>
+					</li>
+					<li>
+						<p>Curso 3</p>
+					</li>
+					<li>
+						<p>Curso 4</p>
+					</li>
+					<li>
+						<p>Curso 5</p>
+					</li>
+				</ul>
 			</div>
+			<Divider orientation='horizontal' variant='middle' />
 			<div>
 		  		{/*Allies*/}
-				<Typography variant='h1'>
-					Allies
+				<Typography variant='h3'>
+					Our Allies
 				</Typography>
+				<br/>
+				<Grid container spacing={2} direction='row' justifyContent='center' sx={{
+					'margin': '10px'
+				}} >
+					<Grid item xs={2}>
+						<Typography sx={{
+							'border': '3px solid #A084DC'
+						}} >
+							Pepito1
+						</Typography>
+					</Grid>
+					<Grid item xs={2}>
+						<Typography sx={{
+							'border': '3px solid #A084DC'
+						}} >
+							Pepito1
+						</Typography>
+					</Grid>
+					<Grid item xs={2}>
+						<Typography sx={{
+							'border': '3px solid #A084DC'
+						}} >
+							Pepito1
+						</Typography>
+					</Grid>
+					<Grid item xs={2}>
+						<Typography sx={{
+							'border': '3px solid #A084DC'
+						}} >
+							Pepito1
+						</Typography>
+					</Grid>
+				</Grid>
 			</div>
+			<Divider orientation='horizontal' variant='fullWidth' />
 		</div>
 	);
 }
