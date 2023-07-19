f5edbdb43448e97e2a8da4da5edf20372ad2912d
Milton Sanchez <snchzmltn@hotmail.com>
2023-07-18 19:58:34 -0500

    update site description, logo and title

--- amsir-page/src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
+++ amsir-page/src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
@@ -5,13 +5,11 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 
 import './ResponsiveAppBar.css';
 import amsirLogo from './../../static/amsirLogoNoBg.png';
