b9b02e87fd5238f49d95cbe57fe4ab798c2a6af0
Milton Sanchez <snchzmltn@hotmail.com>
2023-07-18 23:39:10 -0500

    finish allies conditional rendering based on logo presence. added prop images for dynamic component, the idea is to have the image source dynamically change with the index.

--- amsir-page/src/pages/WelcomePage/WelcomePage.tsx
+++ amsir-page/src/pages/WelcomePage/WelcomePage.tsx
@@ -2,13 +2,18 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
-import { Box, List, ListItem, ListItemButton } from '@mui/material';
+import { Box, List, ListItem, ListItemButton, Stack } from '@mui/material';
 
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 import logoIEEE from './../../static/logo-ieee-white.svg';
 import logoSurvivalMed from './../../static/survival-med-logo.png';
 import logoSumaeUnam from './../../static/SUMAe-UNAM.jpg';
+import sample1 from './../../static/sample-1.jpg';
+import sample2 from './../../static/sample-2.jpg';
+import sample3 from './../../static/sample-3.jpg';
+
+import { useState } from 'react';
 
 class Ally {
 	name: string;
@@ -25,13 +30,17 @@ class Ally {
 const IEEE = new Ally("IEEE", logoIEEE, "https://edu.ieee.org/co-unemb/rama-estudiantil-ieee/");
 const SUR_MED = new Ally("Survival Med", logoSurvivalMed, "https://survivalmedonline.org/courses/wfa-16-hr/?gad=1&gclid=Cj0KCQjw756lBhDMARIsAEI0AgludooHx2qbYK1v0nX4kOd91ZJVYl-p4Zs39SbPG-ButSR63z13de4aAtiYEALw_wcB");
 const SUMAE_UNAM = new Ally("SUMAe-UNAM", logoSumaeUnam, "https://sites.google.com/view/sumae-unam/p%C3%A1gina-principal");
-const PYASMA = new Ally("PYASMA", InstagramIcon, "https://www.instagram.com/pyasmassociation/");
-const CYDONIA = new Ally("CYDONIA", InstagramIcon, "https://www.instagram.com/fundcydonia/");
+const PYASMA = new Ally("PYASMA", null, "https://www.instagram.com/pyasmassociation/");
+const CYDONIA = new Ally("CYDONIA", null, "https://www.instagram.com/fundcydonia/");
 
-const courses = ["Curso 1", "Curso 2", "Curso 3"]
-const allies = [IEEE, SUR_MED, SUMAE_UNAM, PYASMA, CYDONIA]
+const courses = ["Curso 1", "Curso 2", "Curso 3"];
+const coursesImgs = [sample1, sample2, sample3];
+const allies = [IEEE, SUR_MED, SUMAE_UNAM, PYASMA, CYDONIA];
 
 function WelcomePage() {
+
+	const [index, setIndex] = useState(0);
+
 	return (
 		<div>
 			<br />
@@ -43,12 +52,12 @@ function WelcomePage() {
 					What is AMSIR?
 				</Typography>
 				<Typography sx={{
-					'margin-left': '10%',
-					'margin-right': '10%',
-					'padding-left': '1.5em',
-					'padding-right': '1.5em',
-					'border': '5px solid #A084DC',
-					'border-radius': '10%',
+					marginLeft: '10%',
+					marginRight: '10%',
+					paddingLeft: '1.5em',
+					paddingRight: '1.5em',
+					border: '5px solid #A084DC',
+					borderRadius: '10%',
 					':hover': {
 						'background-color': '#fce6f9'
 					}
@@ -57,8 +66,8 @@ function WelcomePage() {
 				</Typography>
 				<br/>
 				<Button sx={{
-					'background-color': '#A084DC',
-					'margin': '0.5em',
+					backgroundColor: '#A084DC',
+					margin: '0.5em',
 					':hover': {
       						'background-color': '#645CBB',
       						'color': 'white',
@@ -82,7 +91,7 @@ function WelcomePage() {
 									<Divider variant='middle' />
 									<ListItem>
 										<ListItemButton>
-											<Typography variant='overline'>
+											<Typography onClick={() => {setIndex(courses.indexOf(course))}} variant='overline'>
 												{course}
 											</Typography>
 										</ListItemButton>
@@ -94,9 +103,7 @@ function WelcomePage() {
 					</Grid>
 					<Grid container item xs={6} md={8}>
 						<Box alignSelf='center' margin='0.1em' width='90%' height='90%' border={'dotted 2.5px'}>
-							<Typography padding='1em' variant='body2'>
-								dynamic description
-							</Typography>
+							<img key={index} src={coursesImgs[index]} alt='courseImg' />
 						</Box>
 					</Grid>
 				</Grid>
@@ -115,17 +122,22 @@ function WelcomePage() {
 						return <>
 							<Grid item xs={2}>
 								<Typography sx={{
-									'background-color': '#A084DC',
-									'padding': '0.5em',
-									'max-width': '50%',
-									'border-radius': '10%',
+									backgroundColor: '#A084DC',
+									padding: '0.5em',
+									maxWidth: '50%',
+									borderRadius: '10%',
 									':hover': {
 										'background-color': '#645CBB',
 										'color': 'white',
 								      	}
 								}} >
 									<a href={ally.url}>
-										<img src={ally.logoImg} alt={ally.name} />
+										{ally.logoImg ?
+											<img src={ally.logoImg} alt={ally.name} />
+											: <Stack alignItems='center' spacing={2}>
+												<InstagramIcon />
+												<p>{ally.name}</p>
+											</Stack>}
 									</a>
 								</Typography>
 							</Grid>
